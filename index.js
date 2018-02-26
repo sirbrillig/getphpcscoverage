61f31b1baa490e6a1a094df50b4762ff0a68f191
Payton Swick <payton@foolord.com>
2018-02-25 22:48:29 -0500

    Move scanner and reporters to their own files

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const readPath = require('recursive-readdir')
 const xml2js = require('xml2js')
 const fs = require('fs')
-const { get, some } = require('lodash')
-const fsPath = require('path')
+const {getXmlReader, getPathReader, getDirectoryScanner} = require('./lib/index')
+const {humanReport, jsonReport} = require('./lib/reporters')
 const meow = require('meow')
 
 const cli = meow(`
@@ -79,85 +79,3 @@ scanDirectory(targetDir, cli.flags)
     console.error(`An error occurred while scanning the directory ${targetDir}:`)
     console.error(err)
   })
-
-function getXmlReader ({readFile, parseXmlString}) {
-  return function getFilesFromXml (xmlFilePath) {
-    return new Promise((resolve, reject) => {
-      readFile(xmlFilePath, (err, data) => {
-        if (err) {
-          return reject(err)
-        }
-        parseXmlString(data, (err, result) => {
-          if (err) {
-            return reject(err)
-          }
-          resolve(get(result, 'ruleset.file', []))
-        })
-      })
-    })
-  }
-}
-
-function getPathReader ({readFilesFromPath}) {
-  return function getFilesFromPath (directoryPath, options) {
-    const ignoreFiles = options.ignore ? [ `*${options.ignore}*` ] : []
-    return readFilesFromPath(directoryPath, ignoreFiles)
-  }
-}
-
-function prependPathToPaths (prepend, paths) {
-  return paths.map(path => fsPath.join(prepend, path))
-}
-
-function filterByFileType (type, paths) {
-  return paths.filter(path => path.endsWith(type))
-}
-
-function filterByParentPaths (parentPaths, paths) {
-  return paths.filter(file => some(parentPaths, parentPath => file.startsWith(parentPath)))
-}
-
-function filterWithoutParentPaths (parentPaths, paths) {
-  return paths.filter(file => !some(parentPaths, parentPath => file.startsWith(parentPath)))
-}
-
-function humanReport (found, notFound) {
-  console.log('These files WILL be scanned by phpcs:')
-  found.map(file => console.log(file))
-  console.log('')
-  console.log('These files WILL NOT be scanned by phpcs:')
-  notFound.map(file => console.log(file))
-  const percent = getPercent(found, notFound)
-  console.log('')
-  console.log(`That's a coverage of ${percent}%`)
-}
-
-function jsonReport (found, notFound) {
-  const percent = getPercent(found, notFound)
-  console.log(JSON.stringify({found, notFound, percent}))
-}
-
-function getPercent (found, notFound) {
-  const totalCount = found.length + notFound.length
-  return Number.parseInt((found.length / totalCount) * 100, 10)
-}
-
-function getDirectoryScanner ({getFilesFromXml, getFilesFromPath}) {
-  return function scanDirectory (directoryPath, options) {
-    const phpcsXmlPath = fsPath.join(directoryPath, 'phpcs.xml')
-    return Promise.all([getFilesFromPath(directoryPath, options), getFilesFromXml(phpcsXmlPath)])
-      .then(results => {
-        const files = filterByFileType(options.type, results[0])
-        const paths = prependPathToPaths(fsPath.dirname(phpcsXmlPath), results[1])
-        const found = filterByParentPaths(paths, files)
-        const notFound = filterWithoutParentPaths(paths, files)
-        return {found, notFound}
-      })
-  }
-}
-
-module.exports = {
-  getDirectoryScanner,
-  getXmlReader,
-  getPathReader
-}
